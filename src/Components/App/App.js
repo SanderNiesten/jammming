45c096dd429044c09ce652ff209284f2b92901b7
SanderNiesten <sanderniesten@live.nl>
2018-01-18 17:38:00 +0100

    setup structure for multiple ccomponents and add some css and html

---
+++ src/Components/App/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
+      </div>
+
+
+//      <div className="App">
+//        <header className="App-header">
+//          <img src={logo} className="App-logo" alt="logo" />
+//          <h1 className="App-title">Welcome to React</h1>
+//        </header>
+//        <p className="App-intro">
+//          To get started, edit <code>src/App.js</code> and save to reload.
+//        </p>
+//      </div>
+    );
+  }
+}
+
+export default App;
