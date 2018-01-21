0756a58364ea96fc1b9b521e9962dfcc39293036
SanderNiesten <sanderniesten@live.nl>
2018-01-21 14:10:44 +0100

    add correct index.html, fix bugs in it, make components show

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <!-- Add a SearchBar component -->
+          <SearchBar />
           <div className="App-playlist">
-            <!-- Add a SearchResults component -->
-            <!-- Add a Playlist component -->
+            <SearchResults />
+//            <Playlist />
           </div>
         </div>
       </div>
-
-
-//      <div className="App">
-//        <header className="App-header">
-//          <img src={logo} className="App-logo" alt="logo" />
-//          <h1 className="App-title">Welcome to React</h1>
-//        </header>
-//        <p className="App-intro">
-//          To get started, edit <code>src/App.js</code> and save to reload.
-//        </p>
-//      </div>
     );
   }
 }
